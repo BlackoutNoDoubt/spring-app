a2c95944c512176672e3dd5ab5ae798f35a119d7
Blackout <sirblackout@yandex.com>
2022-01-11 16:06:55 +0300

    Annotations usage

--- src/main/java/com/blackout/spring/TestSpring.java
+++ src/main/java/com/blackout/spring/TestSpring.java
@@ -6,25 +6,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-//        ClassicalMusic classicalMusic = context.getBean("musicBeanOne", ClassicalMusic.class);
-//        System.out.println(classicalMusic.getSong());
-
-//        Music alternativeRockMusic = context.getBean("alternativeRockMusic", Music.class);
-//        MusicPlayer alternativeRockMusicPlayer = new MusicPlayer(alternativeRockMusic);
-//
-//        Music indieMusic = context.getBean("indieMusic", Music.class);
-//        MusicPlayer indieMusicPlayer = new MusicPlayer(indieMusic);
-
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-//        alternativeRockMusicPlayer.playMusic();
-//        indieMusicPlayer.playMusic();
-//
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
-//
-//        System.out.println(".....");
-//
         musicPlayer.playMusicList();
 
         context.close();
